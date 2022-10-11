efe358eff92fd0fe89d82fe33f59d38261ae0de1
Maurelian <maurelian@protonmail.ch>
2022-10-11 14:25:58 -0500

    ctb: Remove MessagePassedExtension1 event
    The hash argument is moved into the MessagePassed event.
    It is no longer indexed.

--- op-node/withdrawals/utils_test.go
+++ op-node/withdrawals/utils_test.go
@@ -48,10 +48,11 @@ func TestParseMessagePassed(t *testing.T) {
 						"0000000000000000000000000000000000000000000000000000000000000000" +
 						"00000000",
 				),
+				WithdrawalHash: common.HexToHash("0x0d827f8148288e3a2466018f71b968ece4ea9f9e2a81c30da9bd46cce2868285"),
 				Raw: types.Log{
 					Address: common.HexToAddress("0x4200000000000000000000000000000000000016"),
 					Topics: []common.Hash{
-						common.HexToHash("0x7744840cae4793a72467311120512aa98e4398bcd2b9d379b2b9c3b60fa03d72"),
+						common.HexToHash("0x02a52367d10742d8032712c1bb8e0144ff1ec5ffda1ed7d70bb05a2744955054"),
 						common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
 						common.HexToHash("0x0000000000000000000000004200000000000000000000000000000000000007"),
 						common.HexToHash("0x0000000000000000000000006900000000000000000000000000000000000002"),
@@ -112,48 +113,6 @@ func TestParseMessagePassed(t *testing.T) {
 			test.expected.Nonce = nil
 			test.expected.Value = nil
 
-			require.EqualValues(t, test.expected, parsed)
-		})
-	}
-}
-
-func TestParseMessagePassedExtension1(t *testing.T) {
-	tests := []struct {
-		name     string
-		file     string
-		expected *bindings.L2ToL1MessagePasserMessagePassedExtension1
-	}{
-		{
-			"withdrawal through bridge",
-			"bridge-withdrawal.json",
-			&bindings.L2ToL1MessagePasserMessagePassedExtension1{
-				Hash: common.HexToHash("0x0d827f8148288e3a2466018f71b968ece4ea9f9e2a81c30da9bd46cce2868285"),
-				Raw: types.Log{
-					Address: common.HexToAddress("0x4200000000000000000000000000000000000016"),
-					Topics: []common.Hash{
-						common.HexToHash("0xedd348f9c36ef1a5b0747bb5039752707059f0b934c8e508b3271e08fbd0122c"),
-						common.HexToHash("0x0d827f8148288e3a2466018f71b968ece4ea9f9e2a81c30da9bd46cce2868285"),
-					},
-					Data:        []byte{},
-					BlockNumber: 0x36,
-					TxHash:      common.HexToHash("0x9346381068b59d2098495baa72ed2f773c1e09458610a7a208984859dff73add"),
-					TxIndex:     0x1,
-					BlockHash:   common.HexToHash("0xfdd4ad8a984b45687aca0463db491cbd0e85273d970019a3f8bf618b614938df"),
-					Index:       0x3,
-					Removed:     false,
-				},
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			f, err := os.Open(path.Join("testdata", test.file))
-			require.NoError(t, err)
-			dec := json.NewDecoder(f)
-			receipt := new(types.Receipt)
-			require.NoError(t, dec.Decode(receipt))
-			parsed, err := ParseMessagePassedExtension1(receipt)
-			require.NoError(t, err)
 			require.EqualValues(t, test.expected, parsed)
 		})
 	}
